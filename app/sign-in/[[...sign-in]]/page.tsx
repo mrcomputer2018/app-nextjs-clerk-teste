ce9f0bf749c929c4fea7e307cb0b8cba8872c151
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-11-16 10:10:33 -0300

    Refactor sign-in page layout, enhance styling, and migrate utility functions to a new file

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
+
 import * as Clerk from '@clerk/elements/common'
 import * as SignIn from '@clerk/elements/sign-in'
 /* import Link from 'next/link' */
@@ -15,11 +15,10 @@ import {
 import { Input } from '../..//_components/ui/input'
 import { Label } from '../..//_components/ui/label'
 import { Icons } from '../..//_components/ui/icons'
-import { cn } from '../../../lib/utils'
 
 export default function SignInPage() {
     return (
-        <div className="grid w-full grow items-center px-4 sm:justify-center">
+        <div className="grid w-full grow items-center px-4 sm:justify-center h-screen">
             <SignIn.Root>
                 <Clerk.Loading>
                 {(isGlobalLoading) => (
@@ -31,61 +30,14 @@ export default function SignInPage() {
                             <CardDescription>Welcome back! Please sign in to continue</CardDescription>
                         </CardHeader>
                         <CardContent className="grid gap-y-4">
-                            <div className="grid grid-cols-2 gap-x-4">
-                            <Clerk.Connection name="github" asChild>
-                                <Button
-                                size="sm"
-                                variant="outline"
-                                type="button"
-                                disabled={isGlobalLoading}
-                                >
-                                <Clerk.Loading scope="provider:github">
-                                    {(isLoading) =>
-                                    isLoading ? (
-                                        <Icons.spinner className="size-4 animate-spin" />
-                                    ) : (
-                                        <>
-                                        <Icons.gitHub className="mr-2 size-4" />
-                                        GitHub
-                                        </>
-                                    )
-                                    }
-                                </Clerk.Loading>
-                                </Button>
-                            </Clerk.Connection>
-                            <Clerk.Connection name="google" asChild>
-                                <Button
-                                size="sm"
-                                variant="outline"
-                                type="button"
-                                disabled={isGlobalLoading}
-                                >
-                                <Clerk.Loading scope="provider:google">
-                                    {(isLoading) =>
-                                    isLoading ? (
-                                        <Icons.spinner className="size-4 animate-spin" />
-                                    ) : (
-                                        <>
-                                        <Icons.google className="mr-2 size-4" />
-                                        Google
-                                        </>
-                                    )
-                                    }
-                                </Clerk.Loading>
-                                </Button>
-                            </Clerk.Connection>
-                            </div>
-                            <p className="flex items-center gap-x-3 text-sm text-muted-foreground before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border">
-                            or
-                            </p>
                             <Clerk.Field name="identifier" className="space-y-2">
-                            <Clerk.Label asChild>
-                                <Label>Email address</Label>
-                            </Clerk.Label>
-                            <Clerk.Input type="email" required asChild>
-                                <Input />
-                            </Clerk.Input>
-                            <Clerk.FieldError className="block text-sm text-destructive" />
+                                <Clerk.Label asChild>
+                                    <Label>Email address</Label>
+                                </Clerk.Label>
+                                <Clerk.Input type="email" required asChild>
+                                    <Input />
+                                </Clerk.Input>
+                                <Clerk.FieldError className="block text-sm text-destructive" />
                             </Clerk.Field>
                         </CardContent>
                         <CardFooter>
