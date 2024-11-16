a0ccb85e14a13f233cee7c3b4b3222dbc048df3d
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-11-16 08:22:47 -0300

    Enhance authentication flow by updating sign-in page layout, adding sign-up page, and refining middleware for public route handling

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import * as SignIn from '@clerk/elements/sign-in'
 
 export default function SignInPage() {
     return (
-        <div>
-            <SignIn.Root>
-                <SignIn.Step name="start">
-                    <h1>Sign in to your account</h1>
-                </SignIn.Step>
-            </SignIn.Root>
-        </div>
+        <section className=''>
+            <div className='container flex items-center justify-center'>
+                <SignIn.Root>
+                    <SignIn.Step name="start">
+                        <h1>Sign in to your account</h1>
+                    </SignIn.Step>
+                </SignIn.Root>
+            </div>
+        </section>
     );
 }
